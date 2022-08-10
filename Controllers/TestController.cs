de8a1312aef1cf6e5c85fb3c0a76a507253ba5ef
giovann1guillen <105879909+giovann1guillen@users.noreply.github.com>
2022-08-10 17:32:04 -0300

    bhaskara

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -9,4 +9,31 @@ public class TestController : ControllerBase
     {
         return "My App is Working";
     }
+
+    [HttpGet("quadratica{x}")]
+    public string Get(int x, int a = 1, int b = 0, int c = 0)
+    {
+        int resultado = a * x * x + b * x + c;
+        return resultado.ToString();
+    }
+    [HttpGet("solucao")]
+    public object Get(double a  = 1, double  b = 0, double c = 0);
+    {
+        if(a == 0)
+        {
+            return{
+            status = "fail";
+            message = "a nao pode ser 0";
+            };
+        }
+    
+    double delta = b * b - 4 * a * c ;
+        if(delta < 0)
+        {   return{
+            status = "fail";
+            message = "delta nao pode ser negativo"
+            };
+        }
+    double x1 = (-b + math.sqrt(delta)/2*a);
+    double x2 = (-b - math.sqrt(delta)/2*a);
 }
