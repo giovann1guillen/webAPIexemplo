e5e362c5bb5dde6cfca63b81b01bcd7998c1e98f
giovann1guillen <105879909+giovann1guillen@users.noreply.github.com>
2022-08-10 18:56:39 -0300

    formula de bhaskara

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -7,33 +7,44 @@ public class TestController : ControllerBase
     [HttpGet]
     public string Get()
     {
-        return "My App is Working";
+        return "My App is Working!";
     }
 
-    [HttpGet("quadratica{x}")]
+    [HttpGet("quadratica/{x}")]
     public string Get(int x, int a = 1, int b = 0, int c = 0)
     {
         int resultado = a * x * x + b * x + c;
         return resultado.ToString();
     }
+
     [HttpGet("solucao")]
-    public object Get(double a  = 1, double  b = 0, double c = 0);
+    public object Get(
+        double a = 1, double b = 0, double c = 0)
     {
-        if(a == 0)
+        if (a == 0)
         {
-            return{
-            status = "fail";
-            message = "a nao pode ser 0";
+            return new {
+                status = "Fail",
+                message = "'a' não pode ser zero."
             };
         }
-    
-    double delta = b * b - 4 * a * c ;
-        if(delta < 0)
-        {   return{
-            status = "fail";
-            message = "delta nao pode ser negativo"
+
+        double delta = b * b - 4 * a * c;
+        if (delta < 0)
+        {
+            return new {
+                status = "Fail",
+                message = "delta não pode ser negativo"
             };
         }
-    double x1 = (-b + math.sqrt(delta)/2*a);
-    double x2 = (-b - math.sqrt(delta)/2*a);
+
+        double x1 = (-b + Math.Sqrt(delta)) / a;
+        double x2 = (-b - Math.Sqrt(delta)) / a;
+
+        return new {
+            status = "Sucesso",
+            solucao1 = x1,
+            solucao2 = x2
+        };
+    }
 }
